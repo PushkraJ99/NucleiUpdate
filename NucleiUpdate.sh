6fda52a492eab48c630a12234a8dac1e0f1c9516
PushkraJ <104222808+PushkraJ99@users.noreply.github.com>
2023-08-24 01:00:50 +0530

    Update NucleiUpdate.sh

--- NucleiUpdate.sh
+++ NucleiUpdate.sh
@@ -14,12 +14,13 @@ echo -e "\e[91m
 
 # Update Nuclei
 echo "Updatalling Nuclei..."
+sudo cd ~
 sudo cd /usr/local/bin/
 sudo git clone https://github.com/projectdiscovery/nuclei
 sudo cd nuclei/v2/cmd/nuclei; go build
 sudo mv nuclei /usr/bin
 
-sudo cd ../
+sudo cd 
 sudo cd /usr/local/bin/
 sudo rm -r nuclei
 
