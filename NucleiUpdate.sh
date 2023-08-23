faee51098448f8913a9f8b2f8c2d16e31a403310
PushkraJ <104222808+PushkraJ99@users.noreply.github.com>
2023-08-23 18:03:59 +0530

    Update NucleiUpdate.sh

--- NucleiUpdate.sh
+++ NucleiUpdate.sh
@@ -12,21 +12,17 @@ echo -e "\e[91m
                                             Made By PushkraJ
 \e[0m"
 
-# Get the current user's home directory
-home_dir=$(eval echo ~$USER)
 
 # Check if nuclei is Updated, if not, Update it
-    echo "Updatalling Nuclei..."
-    cd Desktop
-    git clone https://github.com/projectdiscovery/nuclei
-    cd nuclei/v2/cmd/nuclei; go build
-    sudo mv nuclei /usr/bin
+echo "Updatalling Nuclei..."
+cd Desktop
+git clone https://github.com/projectdiscovery/nuclei
+cd nuclei/v2/cmd/nuclei; go build
+sudo mv nuclei /usr/bin
 
-    cd
-    cd Desktop
-    sudo rm -r nuclei
-fi
-
+cd ../
+cd Desktop
+sudo rm -r nuclei
 
 # Nuclei Update is completed
 
